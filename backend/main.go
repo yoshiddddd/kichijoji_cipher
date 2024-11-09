234be32e191766d0d738e9db64bfbc56298df967
yoshiddddd <yoshidakerapo@gmail.com>
2024-11-09 11:24:07 +0900

    BEファイル分割

--- backend/main.go
+++ backend/main.go
@@ -5,11 +5,6 @@ import (
     "net/http"
     "sync"
 	 "encoding/json"
-	 "fmt"
-	  "bytes"
-	  "io"
-	  "os"
-	  "github.com/joho/godotenv"
 	  "math/rand"
 	  "time"
     "github.com/gorilla/websocket"
@@ -208,67 +203,7 @@ func (c *Client) readPump(s *Server) {
         countLock.Unlock()
     }
 }
-func sendToDify(data []AnswerMessage) (string, error) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf(".envファイルの読み込みに失敗しました: %v", err)
-	}
-	token :=os.Getenv("DIFY_APIKEY")
-	fmt.Println(token)
-	//送信するクエリの内容はここ
-	query := fmt.Sprintf("keyword: %s name(%s) Answer: %s, name(%s) Answer: %s",
-	data[0].Keyword, data[0].Name, data[0].Answer, data[1].Name, data[1].Answer)
 
-	payload := DifyRequestPayload{
-        Inputs:         map[string]interface{}{}, 
-        Query:          query,
-        ResponseMode:   "blocking",
-        ConversationID: "",         
-        User:           "abc-123",  
-        Files: []File{
-            {
-                Type:           "image",
-                TransferMethod: "remote_url",
-                URL:            "https://cloud.dify.ai/logo/logo-site.png",
-            },
-        },
-    }
-
-	requestBody, err := json.Marshal(payload)
-	// requestBody, err := json.Marshal(data)
-    if err != nil {
-        return "",fmt.Errorf("error encoding data to JSON: %v", err)
-    }
-
-    req, err := http.NewRequest("POST", "https://api.dify.ai/v1/chat-messages", bytes.NewBuffer(requestBody))
-    if err != nil {
-        return "",fmt.Errorf("error creating HTTP request: %v", err)
-    }
-    req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-    req.Header.Set("Content-Type", "application/json")
-    // リクエストの送信
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return "",fmt.Errorf("error sending request to Dify: %v", err)
-    }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusOK {
-        return "",fmt.Errorf("failed to send data to Dify, status code: %d", resp.StatusCode)
-    }
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return "",fmt.Errorf("error reading response body: %v", err)
-    }
-	var difyResponse DifyResponse
-    if err := json.Unmarshal(body, &difyResponse); err != nil {
-        return "",fmt.Errorf("error unmarshalling response: %v", err)
-    }
-
-    // `answer`フィールドの確認とログ出力
-    return  difyResponse.Answer,nil
-}
 
 func serveWs(server *Server, w http.ResponseWriter, r *http.Request) {
     conn, err := upgrader.Upgrade(w, r, nil)
